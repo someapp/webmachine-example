4a7d61d082c194464c62592db17afc17f477c439
root <root@chat.dev0.spark.net>
2013-07-21 14:31:07 -0400

    it cannot detect priv dir

--- src/prp_sup.erl
+++ src/prp_sup.erl
@@ -42,12 +42,13 @@ upgrade() ->
 %% @doc supervisor callback.
 init([]) ->
     Ip = case os:getenv("WEBMACHINE_IP") of false -> "0.0.0.0"; Any -> Any end,
-    {ok, App} = application:get_application(?MODULE),
-    {ok, Dispatch} = file:consult(filename:join([code:priv_dir(App),
-                                                 "dispatch.conf"])),
+    {ok, Cwd} = cwd("priv"),
+    {ok, Dispatch} = file:consult(
+			filename:join([Cwd,"dispatch.conf"])),
+ 
     WebConfig = [
                  {ip, Ip},
-                 {port, 8888},
+                 {port, 8080},
                  {log_dir, "priv/log"},
                  {dispatch, Dispatch}],
     Web = {webmachine_mochiweb,
@@ -55,3 +56,7 @@ init([]) ->
            permanent, 5000, worker, [mochiweb_socket_server]},
     Processes = [Web],
     {ok, { {one_for_one, 10, 10}, Processes} }.
+
+cwd(Path)->
+  {ok, Cwd} = file:get_cwd(),
+  {ok, lists:concat([Cwd,"/",Path])}.
